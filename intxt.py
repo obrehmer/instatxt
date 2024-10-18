c2134dae3c0710f4f9c9af7b4e8bd96ad5c0169a
Olaf Sarnow <olsarnow@gmail.com>
2024-10-18 19:05:50 +0200

    5

--- intxt.py
+++ intxt.py
@@ -13,7 +13,7 @@ year = input("Year (2024): ") or "2024"
 month = input("Month: ") 
 add = input("Additions: ") 
 
-ai_system_message = f"I need your assistance! Write a text for an Instagram post in English, using simple language, in three sentences. The post should be written from the observer's perspective, with a neutral and professional tone. Begin with a sentence mentioning the location and date the picture was taken. Include 20 relevant hashtags, and avoid using the word 'my'; instead, use 'a' or similar alternatives. The context is that the speaker is a photographer. Always start with a sentence that includes the location and date the picture was taken. Start with location, year/month and the Camera model, objective. At least 3 sentences "
+ai_system_message = f"I need your assistance! Write a text for an Instagram post in English, using simple language, in three sentences. The post should be written from the observer's perspective, with a neutral and professional tone. Begin with a sentence mentioning the location and date the picture was taken. Include 20 relevant hashtags, and avoid using the word 'my'; instead, use 'a' or similar alternatives. The context is that the speaker is a photographer. Always start with a sentence that includes the location and date the picture was taken. Start with location, year/month and the Camera model, objective. write 5 sentences "
 
 ai_user_message = f"taken with {cam} with a lens {obj}, in {loc}, in the {month} of  {year},  {add} "
 
